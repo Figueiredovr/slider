69d7c50d844988a4f40ee8ab5da5b7c5ed97945d
Victor Figueiredo <figueiredovr@gmail.com>
2017-03-17 04:57:15 -0300

    App

--- app.js
+++ app.js
@@ -1,4 +1,68 @@
-// app code goes here
-// matrix.init()....
-//
-// have fun
+
+var state = 'init';
+var posx; // posição horizontal da mão
+var currentx;
+var nextstate = 'init';
+
+var options ={
+refresh: 1000 // tempo para reiniciar depois de uma detecção
+};
+
+
+matrix.led([
+          {
+            arc: 270,
+            color: 'red',
+            start: 110
+          }
+        ]).render();
+
+matrix.service('palm',options ).start().then(function locat(data){
+
+  switch (state) {
+    case 'init': //Estado inicial, espera encontrar a posição inicial da mao
+    matrix.led('green').render();
+    posx =  data.location.x;
+    nextstate = 'recog';
+    break;
+
+    case 'recog': // procura a direção do deslocamento da mao
+    if (posx/100 < data.location.x/100) {
+      matrix.led([    //arco de led para direita
+                {
+                  arc: 180,
+                  color: 'blue',
+                  start: 270
+                }
+              ]).render();
+              nextstate = 'delay';
+    } else if (posx/100 > data.location.x/100) {
+      matrix.led([//arco de led para esquerda
+                {
+                  arc: 180,
+                  color: 'blue',
+                  start: 90
+                }
+              ]).render();
+              nextstate = 'delay';
+    }else{
+      matrix.led("purple").render();
+    }
+      break;
+
+      case 'delay': //estado de espera
+      matrix.led('orange').render();
+      nextstate = 'delay2';
+      break;
+
+      case 'delay2': //resetando pra o inicial
+      matrix.led('black').render();
+      nextstate = 'init';
+      break;
+  }
+
+  setTimeout(function () {
+        state = nextstate;
+  }, 500);
+
+});
